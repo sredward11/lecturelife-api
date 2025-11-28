b016460c060a1d7a96588f06a165abcec4c221d6
MsAnaCode <ana.sousa30@iesb.edu.br>
2025-11-28 16:35:09 -0300

    Commit Subindo as adições do index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const authRouter = require('./authRouter');
 const booksRouter = require('./booksRouter');
+const readingsRouter = require('./readingsRouter'); 
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'LectureLife API rodando!' });
 });
-
+   
 router.use('/auth', authRouter);
 router.use('/books', booksRouter);
+router.use('/readings', readingsRouter); 
 
 module.exports = router;
